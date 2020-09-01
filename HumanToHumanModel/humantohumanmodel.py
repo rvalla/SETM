3a281802092c1652ce01347bd6e5c4bf433c5b4c
Rodrigo Valla <rodrigovalla@protonmail.ch>
2020-09-01 00:41:28 -0300

    Running simulaltions...

--- HumanToHumanModel/humantohumanmodel.py
+++ HumanToHumanModel/humantohumanmodel.py
@@ -6,45 +6,45 @@ from processingdata import ProcessingData as dt
 simulationsPeriod = 360
 simulationsPopulation = 5000 #Recommendation: Population > 500 to prevent randomization errors.
 simulationsCount = 3
-simulationsName = "31082020_5K_360d_Gov00dAI"
+simulationsName = "31082020_5K_360d_Opt"
 casesCeroCount = 5 #How many infected humans will be injected in urban area A
 areaBDensity = 1.0 #Population density in area B (relative to area A)
 startingImmunity = 0.0 #The proportion of the population which has immunity before the outbreak
 
 #Control of population behavior in response to the outbreak
-autoIsolationThreshold = 0.0 # [0,1] The probability of a human will auto-isolate himself when having symptoms
-behavior = False #Deciding if the outbreak will change humans behavior
-behaviorTrigger = 0.02 # [0,1] Infected population percentage threshold which trigger behavior factor
-behaviorOff = 0.01 # [0,1] Infected population percentage threshold which deactivates behavior factor
+autoIsolationThreshold = 0.4 # [0,1] The probability of a human will auto-isolate himself when having symptoms
+behavior = True #Deciding if the outbreak will change humans behavior
+behaviorTrigger = 0.004 # [0,1] Infected population percentage threshold which trigger behavior factor
+behaviorOff = 0.002 # [0,1] Infected population percentage threshold which deactivates behavior factor
 behaviorFactor = 1.5 # [1,inf) Improvement of humans' habits while infected population ratio > behaviorTrigger
 
 #Government default response
 activeIsolation = True #Decide if a confirmed case is totally isolated by the government
-activeTracking = False #Decide if government track tested humans close contacts to test and isolate them.
+activeTracking = True #Decide if government track tested humans close contacts to test and isolate them.
 activeTrackingThreshold = 0.3 # [0,1] The probability for government to recognize a closed contact.
 activeTrackingPressureW = 1.0 # [0,inf) Control How the population infected ratio affects active tracking
-testingResponse = 0.09 # [0,1] The probability for a symptomatic human of being tested by the government each day
+testingResponse = 0.056 # [0,1] The probability for a symptomatic human of being tested by the government each day
 testingASResponse = 0.01 # [0,1] The probability for a asymptomatic human of being tested by the government each day
 
 #Government countermeasures
 govActionsMode = "normal" #Can be "normal" or "auto"
-govActionsAutoTrigger =  0.03 # [0,1] Decide the infected population % that triggers auto government actions.
-govActionsAutoOff = 0.02 # [0,1] Decide the infected population % that deactivates auto government actions.
+govActionsAutoTrigger =  0.004 # [0,1] Decide the infected population % that triggers auto government actions.
+govActionsAutoOff = 0.002 # [0,1] Decide the infected population % that deactivates auto government actions.
 startCaseCount = 30 # [1,population) Number of confirmed cases needed to start government actions
 actionsPeriod = 28 # [1, period) Duration for government countermeasures in days
-infoFactor = 1.25 # (1, inf) Value to represent government awareness campaigns
-socialDistanceFactor = 1.5 # (1,inf) Value to represent control of social distance
-isolationFactor = 2.0 # (1,inf) Value to reduce number of human contacts
-exchangeFactor = 2.5 # (1,inf) Value to reduce human interchange between urban areas
+infoFactor = 1.15 # (1, inf) Value to represent government awareness campaigns
+socialDistanceFactor = 1.25 # (1,inf) Value to represent control of social distance
+isolationFactor = 1.42 # (1,inf) Value to reduce number of human contacts
+exchangeFactor = 2.0 # (1,inf) Value to reduce human interchange between urban areas
 lockDown = False #Decide if government close urban areas (human exchange will not exist)
 
 #Government countermeasures failure
 govFailure = False #To set a temporarily suspension of government measures
-govFailureMoment = 5 # [1, actionsPeriod) Day since actions start in which failure occurs: < (actionsPeriod - govFailurePeriod).
-govFailurePeriod = 10 # [1, actionsPeriod - govFailureMoment) Days during the government measures are suspended
+govFailureMoment = 14 # [1, actionsPeriod) Day since actions start in which failure occurs: < (actionsPeriod - govFailurePeriod).
+govFailurePeriod = 5 # [1, actionsPeriod - govFailureMoment) Days during the government measures are suspended
 
 #Deciding to run or not tu run government actions
-runGovActions = False
+runGovActions = True
 govActions = [govActionsMode, govActionsAutoTrigger, govActionsAutoOff, startCaseCount, actionsPeriod, infoFactor, \
 				socialDistanceFactor, isolationFactor, exchangeFactor, lockDown, activeIsolation, activeTracking, \
 				activeTrackingThreshold, activeTrackingPressureW, testingResponse, testingASResponse]
